12371322f00f311add68e4f4411fa84decaa0263
Diego Javier Zea <diegozea@gmail.com>
2018-03-21 17:47:37 +0100

    Write minimal phylosofs.py test

---
+++ test/Test_PhyloSofS.py
@@ -0,0 +1,24 @@
+import unittest
+import subprocess
+import filecmp
+
+class Test_PhyloSofS(unittest.TestCase):
+
+    def test_phylosofs(self):
+        self.assertEqual(subprocess.call([  "python", "phylosofs.py",
+                                            "-mode", "P",
+                                            "-o", "test/tmp/", # It fails with "test/tmp"
+                                            "-inSeq", "dat/JNK3.txt"]), 0)
+        self.assertTrue(filecmp.cmp("test/tmp/treeSearch_532_1.txt",
+                                    "test/data/treeSearch_532_1.txt"),
+                                    "treeSearch_532_1.txt")
+        self.assertTrue(filecmp.cmp("test/tmp/solution_532_1_config0.sum",
+                                    "test/data/solution_532_1_config0.sum"),
+                                    "solution_532_1_config0.sum")
+        self.assertTrue(filecmp.cmp("test/tmp/solution_532_1_config0.info",
+                                    "test/data/solution_532_1_config0.info"),
+                                    "solution_532_1_config0.info")
+
+
+if __name__ == '__main__':
+    unittest.main()
