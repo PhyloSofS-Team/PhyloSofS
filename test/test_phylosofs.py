1c747fb09f5a0b857b4c0392515545dfb65ba5f1
Diego Javier Zea <diegozea@gmail.com>
2018-04-13 18:30:31 +0200

    use o.path.join in tests

--- test/test_phylosofs.py
+++ test/test_phylosofs.py
@@ -3,7 +3,6 @@ import unittest
 import subprocess
 import filecmp
 import os
-import shutil
 
 PATH_TMP = os.path.join("test", "tmp")
 
@@ -14,36 +13,59 @@ if not os.path.isdir(PATH_TMP):
 class Test_PhyloSofS(unittest.TestCase):
 
     def test_phylosofs(self):
-        self.assertEqual(subprocess.call(["python", "phylosofs/phylosofs.py",
-                                          "-P",
-                                          "-o", "test/tmp/",
-                                          # It fails with "test/tmp"
-                                          "--inseq", "dat/JNK3.txt"]), 0)
-        self.assertTrue(filecmp.cmp("test/tmp/treeSearch_532_1.txt",
-                                    "test/data/treeSearch_532_1.txt"),
+        path_phylosofs = os.path.join("phylosofs", "phylosofs.py")
+        path_tmp = os.path.join("test", "tmp/")
+        path_data = os.path.join("dat", "JNK3.txt")
+        command = ["python", path_phylosofs,
+                   "-P",
+                   "-o", path_tmp,
+                   "--inseq", path_data]
+        self.assertEqual(subprocess.call(command), 0)
+        self.assertTrue(filecmp.cmp(os.path.join("test", "tmp",
+                                                 "treeSearch_532_1.txt"),
+                                    os.path.join("test", "data",
+                                                 "treeSearch_532_1.txt")),
                         "treeSearch_532_1.txt")
-        self.assertTrue(filecmp.cmp("test/tmp/solution_532_1_config0.sum",
-                                    "test/data/solution_532_1_config0.sum"),
+        self.assertTrue(filecmp.cmp(os.path.join("test", "tmp",
+                                                 "solution_532_1_config0.sum"),
+                                    os.path.join("test", "data",
+                                                 "solution_532_1_config0.sum")
+                                    ),
                         "solution_532_1_config0.sum")
-        self.assertTrue(filecmp.cmp("test/tmp/solution_532_1_config0.info",
-                                    "test/data/solution_532_1_config0.info"),
+        self.assertTrue(filecmp.cmp(os.path.join("test", "tmp",
+                                                 "solution_532_1_config0.info"
+                                                 ),
+                                    os.path.join("test", "data",
+                                                 "solution_532_1_config0.info")
+                                    ),
                         "solution_532_1_config0.info")
-        self.assertFalse(os.path.isdir("test/tmp/bestTopos"))
-        self.assertFalse(os.path.isdir("test/tmp/betterTrees"))
+        self.assertFalse(os.path.isdir(os.path.join("test", "tmp",
+                                                    "bestTopos")))
+        self.assertFalse(os.path.isdir(os.path.join("test", "tmp",
+                                                    "betterTrees")))
 
     def test_best_topos_and_trees(self):
-        self.assertEqual(subprocess.call(["python", "phylosofs/phylosofs.py",
-                                          "-P",
-                                          "-o", "test/tmp/",
-                                          "-s", "100",
-                                          "--inseq", "dat/JNK3.txt"]), 0)
-        self.assertTrue(os.path.isdir("test/tmp/bestTopos"))
-        self.assertTrue(os.path.isdir("test/tmp/betterTrees"))
-        self.assertGreater(len(os.listdir("test/tmp/bestTopos")), 0)
-        self.assertGreater(len(os.listdir("test/tmp/betterTrees")), 0)
+        path_phylosofs = os.path.join("phylosofs", "phylosofs.py")
+        path_tmp = os.path.join("test", "tmp/")
+        path_data = os.path.join("dat", "JNK3.txt")
+        command = ["python", path_phylosofs,
+                   "-P",
+                   "-o", path_tmp,
+                   "-s", "100",
+                   "--inseq", path_data]
+        self.assertEqual(subprocess.call(command), 0)
+        self.assertTrue(os.path.isdir(os.path.join("test", "tmp",
+                                                   "bestTopos")))
+        self.assertTrue(os.path.isdir(os.path.join("test", "tmp",
+                                                   "betterTrees")))
+        self.assertGreater(len(os.listdir(os.path.join("test", "tmp",
+                                                       "bestTopos"))), 0)
+        self.assertGreater(len(os.listdir(os.path.join("test", "tmp",
+                                                       "betterTrees"))), 0)
 
     def tearDown(self):
         phylosofs.utils.clear_folder(PATH_TMP)
+
 
 if __name__ == '__main__':
     unittest.main()
