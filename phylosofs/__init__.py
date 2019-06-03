f26a661dd59d4eaf1ad33b3cd8afe96ea2be3b76
Diego Javier Zea <diegozea@gmail.com>
2019-06-03 15:18:35 +0200

    update init file

--- phylosofs/__init__.py
+++ phylosofs/__init__.py
@@ -1,1 +1,11 @@
-from . import *
+"""
+PhyloSofS: model the evolution and structural impact of alternative splicing.
+"""
+
+from phylosofs import phylosofs
+from phylosofs import drawForestTranscripts
+from phylosofs import inferPhylo
+from phylosofs import initData
+from phylosofs import modelIsoforms
+from phylosofs import nx_utils
+from phylosofs import utils
